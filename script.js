b026dfbb696b1d6275d9797353de1cd359999d63
Pranav Kale <107506777+PranavKale03@users.noreply.github.com>
2024-03-19 16:42:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo",
+  {
+    y: -50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 4,
+    delay: 4,
+  }
+);
+
+gsap.fromTo(
+  ".name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 4,
+    delay: 4,
+  }
+);
